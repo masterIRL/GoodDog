0badadda2251f539b4d3cdb1a960cfe4ddd004a4
JD <Doriac.jd@hotmail.fr>
2018-03-24 23:24:08 +0100

    Test case for Indexation Controller

---
+++ GooDog/test/testControl/ControlIndexationTest.java
@@ -0,0 +1,25 @@
+package testControl;
+
+import control.ControlVerifierIdentification;
+import model.Indexeur;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControlIndexationTest {
+    @Test
+    public void verifierIdentification() throws Exception {
+        ControlVerifierIdentification verifierAdmin= new ControlVerifierIdentification();
+        assertTrue("Devrais etre true",verifierAdmin.verifierIdentification());
+    }
+
+    @Test
+    public void indexer() throws Exception {
+        Indexeur indexeur = new Indexeur();
+        assertTrue("devrais etre true lors de l'indexion du image",indexeur.indexerImage());
+        assertTrue("devrais renvoyer true lors de l'indexion du son ",indexeur.indexerSon());
+        assertTrue("devrais etre true lors de l'indexion du texte",indexeur.indexerTexte());
+
+    }
+
+}
