7b6d690930dc1eecafbe7aaf58ac719e72f0e5f6
Unknown <Anwarfatayri1997@gmail.com>
2018-04-12 19:30:37 +0200

    ajout indexation

---
+++ GooDog/src/testgraphique/testEcranAdmin.java
@@ -0,0 +1,14 @@
+package testgraphique;
+
+import java.io.IOException;
+
+import vuegraphique.FrameAdmin;
+
+public class testEcranAdmin {
+
+public static void main(String[] args) throws IOException {
+		
+	
+		new FrameAdmin();
+}
+}
--- GooDog/src/vuegraphique/FrameParametre.java
+++
@@ -1,9 +0,0 @@
-package vuegraphique;
-
-import javax.swing.JFrame;
-
-public class FrameParametre extends JFrame {
-
-	private static final long serialVersionUID = 1L;
-
-}
