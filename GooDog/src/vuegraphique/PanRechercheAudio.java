4f1a42c1b06b830cfe383aaa63c0faf86ce154cb
Unknown <Anwarfatayri1997@gmail.com>
2018-04-16 19:30:57 +0200

    update

--- GooDog/src/vuegraphique/PanRechercheAudio.java
+++ GooDog/src/vuegraphique/PanRechercheAudio.java
@@ -6,8 +6,13 @@ import java.awt.TextArea;
 
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.JToolTip;
 
 public class PanRechercheAudio extends JPanel{
 
@@ -15,29 +20,43 @@ public class PanRechercheAudio extends JPanel{
 	
 	private static final long serialVersionUID = 1L;
 	private Box boxMiseEnPageAudio=Box.createVerticalBox();
-	private Box boxNbOcurences=Box.createHorizontalBox();
+	private Box boxNbOccurrences=Box.createHorizontalBox();
 	private Box boxParcourir=Box.createHorizontalBox();
 	private Box boxBareDeRecherche=Box.createHorizontalBox();
-	private TextArea barederecherche=new TextArea();
-	private JSpinner spinnerOcurences=new JSpinner();
+	private TextArea barederecherche=new TextArea("text", 3 , 40 , TextArea.SCROLLBARS_NONE);
+	private JSpinner spinnerOccurrences=new JSpinner();
 	private JButton buttonParcourir =new JButton();
 	private JButton buttonRecherche =new JButton();
 	
+	
 	public void initialisation() {
 		this.setBackground(Color.WHITE);
-		barederecherche.setMaximumSize(new Dimension(650,40));
+		barederecherche.setMaximumSize(new Dimension(650,30));
+		barederecherche.setBackground(Color.WHITE);
+		JLabel texteEspace = new JLabel("   ");
 		buttonRecherche.setText("Recherche");
 	
 		boxBareDeRecherche.add(barederecherche);
+		boxBareDeRecherche.add(texteEspace);
 		boxBareDeRecherche.add(buttonRecherche);
 		
 		buttonParcourir.setText("Parcourir");
+		boxParcourir.add(buttonParcourir);
+		
+		
+		JLabel texteNbOccurrences = new JLabel("Nombre d'occurrences :   ");
+		spinnerOccurrences.setMaximumSize(new Dimension(30,30));
+		boxNbOccurrences.add(texteNbOccurrences);
+		boxNbOccurrences.add(spinnerOccurrences);
+		
 		
 		
 		boxMiseEnPageAudio.add(Box.createRigidArea(new Dimension(0,30)));
 		boxMiseEnPageAudio.add(boxBareDeRecherche);
 		boxMiseEnPageAudio.add(Box.createRigidArea(new Dimension(0,30)));
+		boxMiseEnPageAudio.add(boxParcourir);
 		boxMiseEnPageAudio.add(Box.createRigidArea(new Dimension(0,30)));
+		boxMiseEnPageAudio.add(boxNbOccurrences);
 		this.add(boxMiseEnPageAudio);
 		
 	}
