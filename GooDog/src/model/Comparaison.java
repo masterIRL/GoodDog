acc5ad13c7d8becdf3de78cfd49e1f04c59ebacd
masterIRL <34350853+masterIRL@users.noreply.github.com>
2018-04-16 15:46:23 +0200

    up

--- GooDog/src/model/Comparaison.java
+++ GooDog/src/model/Comparaison.java
@@ -12,10 +12,10 @@ public class Comparaison {
 		
 		//Simulation resultat
 		List<String> listTexte = new ArrayList<>();
-		listTexte.add("chamions_league.xml");
-		listTexte.add("regles_du_football.xml");
-		listTexte.add("transfert_de_Neymar.xml");
-		listTexte.add("europa_league.xml");
+		listTexte.add("05-Edition___les_mots_et.xml");
+		listTexte.add("05-Le_Colombien_Juan_Pablo_Montoya.xml");
+		listTexte.add("15-Capturer_et_emprisonner_le_CO.xml");
+		listTexte.add("27-Le_Stade_de_France_s_ouvre.xml");
 		return listTexte;
 	}
 
@@ -24,10 +24,8 @@ public class Comparaison {
 		
 		//Simulation resultat
 		List<String> listSon = new ArrayList<>();
-		listSon.add("zouk.wav");
-		listSon.add("sonerie1.wav");
-		listSon.add("sonnerie2 .wav");
-		listSon.add("radio_ups .wav");
+		listSon.add("corpus_fi.wav");
+		listSon.add("jingle_fi.wav");
 		return listSon;
 	}
 
@@ -36,15 +34,58 @@ public class Comparaison {
 		
 		//Simulation resultat
 		List<String> listImage = new ArrayList<>();
-		listImage.add("1asri.png");
-		listImage.add("PGE.png");
-		listImage.add("YOUPSSITECH.png");
-		listImage.add("ups.png");
+		listImage.add("10.jpg");
+		listImage.add("20.jpg");
+		listImage.add("30.jpg");
+		listImage.add("40.jpg");
 		return listImage;
 	}
 
 	public List<String> comparaisonCouleur(int i, int pourcentage) {
 		// TODO Auto-generated method stub
+		
+		//simulation
+		switch(i) {
+		case 63:
+			List<String> imageBlanc = new ArrayList<>();
+			imageBlanc.add("18.jpg");//32, 35, vert
+			imageBlanc.add("26.jpg");//14, 16,17,38 rouge
+			imageBlanc.add("27.jpg");//13,22 bleu
+			imageBlanc.add("47.jpg");
+			return imageBlanc;
+			
+		case 48:
+			List<String> imageRouge = new ArrayList<>();
+			imageRouge.add("14.jpg");
+			imageRouge.add("16.jpg");//13,22 bleu
+			imageRouge.add("17.jpg");//14, 16,17,38 rouge
+			imageRouge.add("38.jpg");//32, 35, vert
+			return imageRouge;
+			
+		case 12:
+			List<String> imageVert = new ArrayList<>();
+			imageVert.add("32.jpg");
+			imageVert.add("33.jpg");//13,22 bleu
+			imageVert.add("34.jpg");//14, 16,17,38 rouge
+			imageVert.add("35.jpg");//32, 35, vert
+			return imageVert;
+			
+		case 3:
+			List<String> imageBleu = new ArrayList<>();
+			imageBleu.add("13.jpg");
+			imageBleu.add("20.jpg");//14, 16,17,38 rouge
+			imageBleu.add("21.jpg");//32, 35, vert
+			imageBleu.add("46.jpg");//13,22 bleu
+			return imageBleu;
+			
+		case 0:
+			List<String> imagesNoire = new ArrayList<>();
+			imagesNoire.add("04.jpg");
+			imagesNoire.add("12.jpg");//13,22,49 bleu
+			imagesNoire.add("08.jpg");//14, 16,17,38 rouge
+			imagesNoire.add("11.jpg");//32, 35, vert
+			return imagesNoire;
+		}
 		return null;
 	}
 
@@ -58,6 +99,10 @@ public class Comparaison {
 		listMotCle.put("TheWeeknd",8);
 
 		return listMotCle;
+	}
+	
+	public List<String> comparaisonAudio(String nom, int occurence){
+		return null;
 	}
 
 }
