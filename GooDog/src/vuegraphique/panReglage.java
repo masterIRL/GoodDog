bb9ce049fd3a55ac098e1f95d9617d93fcf4ccca
CharlesHL75 <37771551+CharlesHL75@users.noreply.github.com>
2018-04-17 00:38:56 +0200

    +update

--- GooDog/src/vuegraphique/panReglage.java
+++ GooDog/src/vuegraphique/panReglage.java
@@ -1,8 +1,61 @@
 package vuegraphique;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
+
+
 public class panReglage extends JPanel {
+
+	private static final long serialVersionUID = -7586081042841575443L;
+	Box boxMiseEnPageReglage = Box.createVerticalBox();
+	JButton boutonConfig = new JButton();
+	JButton boutonIndexation = new JButton();
+	JButton boutonMode = new JButton();
+
+
+
 	
+	public panReglage () {
+		boutonConfig.setText("CONFIGURATION DE L'INDEXATION");
+		boutonConfig.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		boutonIndexation.setText("INDEXER");
+		boutonConfig.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		boutonIndexation.setText("Changer de mode");
+		boutonConfig.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		
+		boxMiseEnPageReglage.add(Box.createRigidArea(new Dimension(0,50)));
+		boxMiseEnPageReglage.add(boutonConfig);
+		boxMiseEnPageReglage.add(Box.createRigidArea(new Dimension(0,50)));
+		boxMiseEnPageReglage.add(boutonIndexation);
+		boxMiseEnPageReglage.add(Box.createRigidArea(new Dimension(0,50)));
+		boxMiseEnPageReglage.add(boutonMode);
+		boxMiseEnPageReglage.add(Box.createRigidArea(new Dimension(0,50)));
+
+		boxMiseEnPageReglage.setVisible(true);
+
+		
+	}
+
 
 }
